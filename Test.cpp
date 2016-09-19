0fe65222f2ed11435013c6b56aaf9c3f46e8cfde
Simen Fossnes <simenf13@student.uia.no>
2016-09-19 17:53:27 +0200

    Added functionality to test class.

--- Test.cpp
+++ Test.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "Test.h"
+#include <iostream>
+
+using namespace std;
+
+// declaring the start number of Students
+int Test::testNum = 1;
+
+// Constructor
+Test::Test(string course, string test, string date){
+    this -> id = testNum;
+    this -> cn = course;
+    this -> tn = test;
+    this -> dt = date;
+    Test::testNum++;
+}
+Test::Test(){}
+
+// Destructor
+Test::~Test(){
+    cout << this->cn << this->tn << " was destroyed" << endl;
+}
+
+// Test Info method
+void Test::testInfo(){
+    cout << "test id = " << this->id << ", course name = " << this->cn << ", test name = " << this->tn << ", date = " << this->dt << endl;
+}
+
+// Edit Test
+void Test::editTest(string course, string test, string date) {
+    this -> cn = course;
+    this -> tn = test;
+    this -> dt = date;
+}
--- Test.h
+++ Test.h
@@ -5,9 +5,37 @@
 #ifndef HAND_IN_3_1_TEST_H
 #define HAND_IN_3_1_TEST_H
 
+#include <iostream>
+
 
 class Test {
+private:
+    // variables
+    int id;
+    string cn;  // course name
+    string tn;  // test name
+    string dt;
 
+
+    static int testNum;
+
+public:
+    // getter methods
+    int getId(){ return id; }
+    string getCourseName(){ return cn; }
+    string getTestName(){ return tn; }
+    string getDateTime(){ return dt; }
+
+    // setter methods
+    void editTest(string, string, string);
+
+    Test(string, string, string);
+    Test();
+
+    ~Test();
+
+    // return the information about an object
+    void testInfo();
 };
 
 
