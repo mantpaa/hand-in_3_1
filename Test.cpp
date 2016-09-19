376fd4bff6c8ee3f9f8aa5ed260a80229801b5aa
Simen Fossnes <simenf13@student.uia.no>
2016-09-19 17:49:10 +0200

    Initial commit

---
+++ Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by y50-70 on 9/19/2016.
+//
+
+#include "Test.h"
---
+++ Test.h
@@ -0,0 +1,14 @@
+//
+// Created by y50-70 on 9/19/2016.
+//
+
+#ifndef HAND_IN_3_1_TEST_H
+#define HAND_IN_3_1_TEST_H
+
+
+class Test {
+
+};
+
+
+#endif //HAND_IN_3_1_TEST_H
